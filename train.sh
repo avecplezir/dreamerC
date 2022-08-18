564b395e1649660e1570e6a99afc8eafb9bf6c13
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-08-18 19:20:01 +0400

    fix atary

--- train.sh
+++ train.sh
@@ -6,6 +6,6 @@ export WANDB_API_KEY="df61f407e5d9259d358ba2a7ef24aa3038bec740"
 #--logdir ~/logdir/dmc_walker_walk/dynamics/1 \
 #--configs dmc_vision --task dmc_walker_walk
 
-CUDA_VISIBLE_DEVICES=2 python3 dreamerv2/train.py \
+CUDA_VISIBLE_DEVICES=0 python3 dreamerv2/train.py \
 --logdir ~/logdir/atari_pong/dynamics/1 \
 --configs atari --task atari_pong
